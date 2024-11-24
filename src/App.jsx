d5b0baecde4c4a5e361730786f4cfca56e3a3010
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-24 15:30:44 -0500

    start bento setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import AboutSection from './components/AboutSection'
 import Navbar from './components/Navbar'
+import Features from './components/Features'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <AboutSection />
+      <Features />
     </main>
   )
 }
