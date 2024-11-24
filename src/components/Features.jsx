612245c5c157be13416c51715361caf38e2716b7
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-24 16:01:44 -0500

    start tilt functionality

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,8 +2,30 @@ import React, { useRef, useState } from 'react'
 import { TiLocationArrow } from 'react-icons/ti'
 
 const BentoTilt = ({children, className = ""}) => {
+    const [transformStyle, setTransformStyle] = useState("")
+    const itemRef = useRef()
+
+    const handleMouseMove = (e) => {
+        if(!itemRef.current) return
+
+        const {left, top, width, height} = itemRef.current.getBoundingClientRect() // This method retrieves the size and position of an element relative to the viewport.
+
+        const relativeX = (e.clientX - left) / width
+        const relativeY = (e.clientY - top) / height
+
+        const tiltX = (relativeY - 0.5) * 5
+        const tiltY = (relativeX - 0.5) * -5
+
+        const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.95, 0.95, 0.95)`
+        setTransformStyle(newTransform)
+    }
+
+    const handleMouseLeave = () => {
+        setTransformStyle("")
+    }
+
     return (
-        <div>
+        <div ref={itemRef} className={className} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{transform: transformStyle}}>
             {children}
         </div>
     )
