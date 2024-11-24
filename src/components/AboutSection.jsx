f14ecd879944127665599cebe2c872f631777ed0
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-24 01:00:54 -0500

    animated title

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,6 +2,7 @@ import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import React from 'react'
 import ScrollTrigger from 'gsap/all'
+import AnimatedTitle from "./AimatedTitle"
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -29,32 +30,33 @@ const AboutSection = () => {
     return (
         <div id="about" className="min-h-screen w-screen">
             <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-            <p className="font-general text-sm uppercase md:text-[10px]">
-                Welcome to Zentry
-            </p>
-
-            <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-                Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure
-            </div>
-
-
-            <div className="about-subtext">
-                <p>The Game of Games begins—your life, now an epic MMORPG</p>
-                <p className="text-gray-500">
-                Zentry unites every player from countless games and platforms, both
-                digital and physical, into a unified Play Economy
+                <p className="font-general text-sm uppercase md:text-[10px]">
+                    Welcome to Zentry
                 </p>
-            </div>
-            </div>
 
-            <div className="h-dvh w-screen" id="clip">
-            <div className="mask-clip-path about-image">
-                <img
-                src="img/about.webp"
-                alt="Background"
-                className="absolute left-0 top-0 size-full object-cover"
+                <AnimatedTitle
+                    title="Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure"
+                    containerClass="mt-5 !text-black text-center"
                 />
-            </div>
+
+
+                <div className="about-subtext">
+                    <p>The Game of Games begins—your life, now an epic MMORPG</p>
+                    <p className="text-gray-500">
+                    Zentry unites every player from countless games and platforms, both
+                    digital and physical, into a unified Play Economy
+                    </p>
+                </div>
+                </div>
+
+                <div className="h-dvh w-screen" id="clip">
+                <div className="mask-clip-path about-image">
+                    <img
+                    src="img/about.webp"
+                    alt="Background"
+                    className="absolute left-0 top-0 size-full object-cover"
+                    />
+                </div>
             </div>
         </div>
     );
